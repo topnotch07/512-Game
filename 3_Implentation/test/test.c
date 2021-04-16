86de6fede12d1aad888ba0408611f317680ecdf0
THIRUVIKRAMAN B <thiruvikraman07@gmail.com>
2021-04-16 15:02:18 +0530

    Update test.c

--- 3_Implentation/test/test.c
+++ 3_Implentation/test/test.c
@@ -29,97 +29,18 @@ int main()
 
 /* Write all the test functions */ 
 void starting() {
-    int i;
-    char sentence1[]="WELCOME NERD";
-    char sentence2[]="Press any key to continue";
-    char sentence3[]="sliding block puzzle game";
-
-    char sentence4[]="WELCOME TO 512 GAME IN 'C'";
-
-
-
-    waitL();
-    printf("\n\n\n\n\n\n\n\t\t\t\t");
-    printer(sentence1);
-    waitL();
-    waitL();
-    printf("\n\n\n\n\n\n\n\n\t\t\t\t\t\t");
-    printer(sentence2);
-  
-
-    printf("\n\n\n\n\t\t\t\t\t");
-    printer(sentence4);
-    waitL();
-    printf("\n\n\n\n\t\t\t\t\t");
-    waitL();
-    printer(sentence3);
-    waitL();
-    waitL();
-    system("cls");
-
-    printf("\n\n\t\t\t\t\t");
-    for(i=0;i<10;i++)
-    {
-        waitL();
-    }
-    system("cls");
-    printf("\n\n\n\t\t\tINSTRUCTION\n\n\n");
-    printf(" ->  Enter arrow key to move\n\n");
-    printf(" ->  For wining this game any one box have value 2048\n\n");
-    printf(" -> You have maximum 512 try to win the game\n\n\n\n");
-    printf("\t\t\t\t\t\tPRESS ANY KEY TO PLAY\n");
-
+     TEST_ASSERT_EQUAL(SUCCESS,,);
 }
 
-void Display() { 
-    int i,j;
-    system("cls");
-
-    for(i=0;i<SIZE;i++)
-    {
-        printf("\t\t\t %c",BORDER);
-        for(j=0;j<SIZE;j++)
-        {
-            if(Matrix[i][j]==0)
-            {
-                printf("      ");
-            }
-            else
-                printf(" %4d ",Matrix[i][j]);
-        }
-        printf("%c\n\t\t\t %c                        %c\n",BORDER,BORDER,BORDER);
-    }
-    printf("\t\t\t %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n\n",BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER,BORDER);
+void Display() {
+     TEST_ASSERT_EQUAL(SUCCESS,,);
 }
 
 void Up() {
-    int i,j;
-    for(j=0;j<SIZE;j++)
-    {
-        i=1;
-        while(1)
-        {
-            while(Matrix[i][j]==0)
-            {
-                if(i==SIZE)
-                    break;
-                i++;
-            }
-            if(i==SIZE)
-                break;
-            while(i>0 && (Matrix[i-1][j]==0 || Matrix[i][j]==Matrix[i-1][j]))
-            {
-                Matrix[i-1][j]+=Matrix[i][j];
-                Matrix[i][j]=0;
-                i--;
-            }
-            i++;
-        }
-    }
+         TEST_ASSERT_EQUAL(SUCCESS,,);
+
 }
 
 void wait() {
-    int i;
-    for(i=0;i<4500000;i++);
-  
+        TEST_ASSERT_EQUAL(SUCCESS,,);  
 }
